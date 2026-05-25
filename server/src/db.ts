3dadde0a2810dcba41e53046a6d45a7302c6a955
cedlaro <cedlaro@gmail.com>
2026-05-25 19:28:27 +0400

    db

--- server/src/db.ts
+++ server/src/db.ts
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 
 export async function query<T>(sql: string, params: (string | number | null | boolean | Date)[] = []): Promise<T> {
 console.log({
-   process.env.MYSQL_HOST,
+   host:process.env.MYSQL_HOST
 });
 
   const connection = await mysql.createConnection({
