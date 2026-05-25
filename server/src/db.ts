6b226669ca276887f6f799906e8d61145792a9c4
cedlaro <cedlaro@gmail.com>
2026-05-25 17:08:30 +0400

    db

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,19 +1,13 @@
 import mysql from 'mysql2/promise';
 
 export async function query<T>(sql: string, params: (string | number | null | boolean | Date)[] = []): Promise<T> {
-console.log({
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_USER: process.env.DB_USER,
-  DB_NAME: process.env.DB_NAME,
-});
 
   const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || '3306'),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT || '3306'),
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_NAME,
   });
 
   try {
